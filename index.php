617baaa521e40b8522d2301a745f174de1874e2d
Muhammad Alfian <alfian.spr@gmail.com>
2026-02-04 16:59:26 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>Hello World</h1>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi nemo sapiente, quis autem voluptates aliquid minus dolores nam nisi, aspernatur hic maxime, ipsum at quidem iure placeat accusantium earum! Architecto.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
